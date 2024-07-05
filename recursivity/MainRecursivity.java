39300bcce35515fa810db501021e582ff076e97f
edrikfsteiner <163454021+edrikfsteiner@users.noreply.github.com>
2024-07-05 14:19:25 -0300

    Create recursivity

---
+++ recursivity/MainRecursivity.java
@@ -0,0 +1,21 @@
+package javaFunctions.recursivity;
+import java.util.List;
+
+public class MainRecursivity {
+    public static void main(String[] args) {
+        // Uncomment the code below to run the functions
+        /*
+        recursivity fac = new recursivity();
+        int res = fac.factorial(5);
+        System.out.println(res);
+
+        recursivity nat = new recursivity();
+        res = nat.nat_sum(10);
+        System.out.println(res);
+
+        recursivity fib = new recursivity();
+        List<Integer> seq = fib.fibonacci(10);
+        System.out.println(seq);
+        */
+    }
+}
---
+++ recursivity/recursivity.java
@@ -0,0 +1,42 @@
+package javaFunctions.recursivity;
+import java.util.ArrayList;
+import java.util.List;
+
+public class recursivity {
+    public int factorial(int value) {
+        if (value <= 1) {
+            return value;
+        } else {
+            return value * factorial(value - 1);
+        }
+    }
+
+    public int nat_sum(int value) {
+        if (value == 0) {
+            return value;
+        } else {
+            return value + nat_sum(value - 1);
+        }
+    }
+
+    public List<Integer> fibonacci(int value) {
+        List<Integer> fib = new ArrayList<>();
+
+        if (value <= 0) {
+            return fib;
+        } else if (value == 1) {
+            fib.add(0);
+            return fib;
+        } else {
+            fib.add(0);
+            fib.add(1);
+
+            for (int i = 2; i < value; i++) {
+                int nextFib = fib.get(i - 1) + fib.get(i - 2);
+                fib.add(nextFib);
+            }
+
+            return fib;
+        }
+    }
+}
