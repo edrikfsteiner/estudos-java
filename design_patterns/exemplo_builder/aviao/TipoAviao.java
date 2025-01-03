9100604f769a2642a3338c866ddce841d39b4c0a
edrikfsteiner <163454021+edrikfsteiner@users.noreply.github.com>
2025-01-03 14:23:54 -0300

    Delete design_patterns directory

--- design_patterns/exemplo_builder/aviao/TipoAviao.java
+++
@@ -1,5 +0,0 @@
-package javaFunctions.design_patterns.exemplo_builder.aviao;
-
-public enum TipoAviao {
-    CARGA, CIVIL, JATO
-}
--- design_patterns/exemplo_factory/product/Pizza.java
+++
@@ -1,7 +0,0 @@
-package javaFunctions.design_patterns.exemplo_factory.product;
-
-public interface Pizza {
-    String borda = null;
-    String cobertura = null;
-    String tempeiro = null;
-}
